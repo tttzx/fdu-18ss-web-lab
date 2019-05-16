a609597d9338b90bab364a24825313729b2ced6b
tttzx <18301010061@fudan.edu.cn>
2019-05-16 17:21:19 +0800

    lab8

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -2,7 +2,11 @@
 
     function outputOrderRow($file, $title, $quantity, $price) {
         echo "<tr>";
-        //TODO
+        echo "<td><img src='images/books/tinysquare/".$file."'></td>";
+        echo "<td class='mdl-data-table__cell--non-numeric'>".$title."</td>";
+        echo "<td>".$quantity."</td>";
+        echo "<td>$".$price."</td>";
+        echo "<td>$".$quantity*$price."</td>";
         echo "</tr>";
     }
 ?>
--- lab8/lab8.php
+++ lab8/lab8.php
@@ -9,9 +9,13 @@ $shippingFlatAmount = 200;
 // 计算 subtotal, shipping, grand total
 // grand total = subtotal + shipping
 // 如果subtotal值超过阈值，则为100；反之则为$shippingFlatAmount
-$subtotal = 0;
-$shipping = 0;
-$grandTotal = 0;
+$subtotal =$quantity1*$price1+$quantity2*$price2+$quantity3*$price3+$quantity4*$price4;
+if($subtotal>$shippingThreshold):
+    $shipping =100;
+else:
+    $shipping =$shippingFlatAmount;
+endif;
+$grandTotal = $subtotal+$shipping;
 
 ?>
 
@@ -35,8 +39,11 @@ $grandTotal = 0;
 <body>
     
 <!-- You should decide where to add the `header.inc.php` and `left.inc.php` -->
+
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-drawer
             mdl-layout--fixed-header">
+    <?php include 'header.inc.php'; ?>
+    <?php include 'left.inc.php'; ?>
 
   <main class="mdl-layout__content mdl-color--grey-50">
     <header class="mdl-color--blue-grey-200">
